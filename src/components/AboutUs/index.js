4715e225bb1f0eaba286eb3c359ac80b7fe77da8
FreddyObrein <freddyobrein@gmail.com>
2021-10-06 12:28:27 +0200

    added about us page

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -1,24 +1,18 @@
-import axios from 'axios';
 import React from 'react';
 
 class AboutUs extends React.Component {
-    constructor(props){
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    componentDidMount(){
-        axios.get('http://localhost/bedrock/web/wp-json/wp/v2/pages')
-         .then(res => {
-             const pages = res.data;
-             console.log(pages);
-         })
-    }
-
-    render() {
-        return (
-            <div></div>
-        )
-    }
+  render() {
+    // console.log(this.props.content);
+    return (
+      <div>
+        <p className="text-center">{this.props.content}</p>
+      </div>
+    )
+  }
 }
 
 export default AboutUs;
---
+++ src/components/Page/index.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import React from 'react';
+import AboutUs from '../AboutUs';
+
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pages: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost/bedrock/web/wp-json/wp/v2/pages')
+    .then(res => {
+      const pages = res.data;
+      this.setState({ pages })
+      console.log(pages);
+    })
+  }
+  
+  render() {
+    const pages = this.state.pages.map(page => <AboutUs key={page.id} page={page} content={page.content.rendered} dangerouslySetInnerHTML={{__html: page.content}} />)
+
+    return (
+      <div>
+        <div >
+          { pages }
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Page;
