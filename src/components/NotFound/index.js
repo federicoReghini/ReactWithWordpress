5cf43eea2cb2c04d9e9c82161b32927d49779af5
FreddyObrein <freddyobrein@gmail.com>
2021-10-07 12:57:22 +0200

    added NotFound path

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class NotFound extends React.Component {
+  render() {
+    return (
+      <div class="alert alert-danger" role="alert">
+        Not found!
+      </div>
+    );
+  }
+}
